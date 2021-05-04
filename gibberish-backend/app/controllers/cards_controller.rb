48811cab5ec2b020406f95c9cb0099bd34b47643
Kevin <kevhines@gmail.com>
2021-05-04 14:18:49 -0400

    begins ability to patch card name

--- gibberish-backend/app/controllers/cards_controller.rb
+++ gibberish-backend/app/controllers/cards_controller.rb
@@ -4,4 +4,17 @@ class CardsController < ApplicationController
         cards = Card.all 
         render json: cards   #, key_transform: :camel_lower
     end
+
+    def update
+        card = Card.find_by(id: params[:id])
+        card.update(card_params)
+    end
+
+    private
+
+    def card_params
+        params.require(:card).permit(:id, :name)
+    end
+
+
 end
