0e4a47ef87c4c4749016ae498eb940bff6de3dae
Kevin <kevhines@gmail.com>
2021-05-05 13:59:14 -0400

    creates new rule

--- gibberish-backend/app/controllers/rules_controller.rb
+++ gibberish-backend/app/controllers/rules_controller.rb
@@ -1,2 +1,14 @@
 class RulesController < ApplicationController
+
+    def create
+        rule = Rule.new(rule_params)
+        rule.save
+        render json: rule
+    end
+
+    private
+
+    def rule_params
+        params.require(:rule).permit(:rule, :winner_id, :loser_id)
+    end
 end
